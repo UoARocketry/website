fb6163b9915fff35b88c6382625b7a19deb8d559
eyessha <abab484@aucklanduni.ac.nz>
2024-12-20 21:55:04 +1300

    Created eventsData.ts containing shared even data

---
+++ src/app/events/eventsData.ts
@@ -0,0 +1,18 @@
+const events = [
+  {
+    id: "1",
+    title: "Rocket Launch",
+    date: "20/12/2024",
+    time: "9:00",
+    location: "Silverdale, Auckland, New Zealand",
+  },
+  {
+    id: "2",
+    title: "Rocket Launch",
+    date: "21/12/2024",
+    time: "10:00",
+    location: "Silverdale, Auckland, New Zealand",
+  },
+];
+
+export default events;
