4e567d614733fb1cf7447c5e89e2dcfd790da6b0
eyessha <abab484@aucklanduni.ac.nz>
2024-12-25 00:51:11 +1300

    Added past events

--- src/app/events/eventsData.ts
+++ src/app/events/eventsData.ts
@@ -1,18 +1,36 @@
-const events = [
-  {
-    id: "1",
-    title: "Rocket Launch",
-    date: "20/12/2024",
-    time: "9:00",
-    location: "Silverdale, Auckland, New Zealand",
-  },
-  {
-    id: "2",
-    title: "Rocket Launch",
-    date: "21/12/2024",
-    time: "10:00",
-    location: "Silverdale, Auckland, New Zealand",
-  },
-];
+const events = {
+  upcoming: [
+    {
+      id: 1,
+      title: "Rocket Launch",
+      date: "20/12/2024",
+      time: "9:00",
+      location: "Silverdale, Auckland, New Zealand",
+    },
+    {
+      id: 2,
+      title: "Rocket Launch",
+      date: "21/12/2024",
+      time: "10:00",
+      location: "Silverdale, Auckland, New Zealand",
+    },
+  ],
+  past: [
+    {
+      id: 3,
+      title: "Rocket Launch",
+      date: "24/12/2024",
+      time: "9:00",
+      location: "Weichurch, Connecticut, USA",
+    },
+    {
+      id: 4,
+      title: "Rocket Launch",
+      date: "23/12/2024",
+      time: "10:00",
+      location: "Weichurch, Connecticut, USA",
+    },
+  ],
+};
 
 export default events;
