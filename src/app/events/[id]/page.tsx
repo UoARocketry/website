205aa9c4d2ee3443720991d96ceed12894d21c84
eyessha <abab484@aucklanduni.ac.nz>
2024-12-25 02:02:12 +1300

    Made a box around the clicky thing after events

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -19,31 +19,77 @@ export default function EventDetails({ params }: { params: { id: string } }) {
   return (
     <div
       style={{
-        padding: "20px",
-        color: "#fff",
+        padding: "40px",
         backgroundColor: "#1e1e1e",
-        textAlign: "center",
+        color: "#fff",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
       }}
     >
-      <h1 style={{ color: "#FF4500", marginBottom: "20px" }}>{event.title}</h1>
-      <p>Date: {event.date}</p>
-      <p>Time: {event.time}</p>
-      <p>Location: {event.location}</p>
-      <p style={{ marginTop: "40px" }}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-        elementum et felis sed fringilla. Morbi nec tincidunt dolor.
-      </p>
-      <Link
-        href="/events"
+      <div
         style={{
-          color: "#FF4500",
-          textDecoration: "underline",
-          marginTop: "20px",
-          display: "inline-block",
+          maxWidth: "1200px",
+          width: "100%",
+          backgroundColor: "#333",
+          borderRadius: "10px",
+          overflow: "hidden",
+          display: "flex",
         }}
       >
-        Back to Events
-      </Link>
+        {/* Left: Image Placeholder */}
+        <div
+          style={{
+            flex: "1",
+            backgroundColor: "#ccc",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <span style={{ color: "#777" }}>Image Placeholder</span>
+        </div>
+
+        {/* Right: Event Details */}
+        <div
+          style={{
+            flex: "2",
+            padding: "40px",
+          }}
+        >
+          <h1 style={{ color: "#FF4500", marginBottom: "20px" }}>
+            {event.title}
+          </h1>
+          <p style={{ fontSize: "18px", marginBottom: "10px" }}>
+            <strong>Time:</strong> {event.time}
+          </p>
+          <p style={{ fontSize: "18px", marginBottom: "10px" }}>
+            <strong>Date:</strong> {event.date}
+          </p>
+          <p style={{ fontSize: "18px", marginBottom: "20px" }}>
+            <strong>Location:</strong> {event.location}
+          </p>
+          <p style={{ lineHeight: "1.6", marginBottom: "20px" }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            elementum et felis sed fringilla. Morbi nec tincidunt dolor. Auctor
+            pharetra at nascetur cras id imperdiet vehicula. Vivamus elementum
+            et felis sed fringilla.
+          </p>
+
+          <Link
+            href="/events"
+            style={{
+              color: "#FF4500",
+              textDecoration: "underline",
+              display: "inline-block",
+              marginTop: "20px",
+            }}
+          >
+            Back to Events
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
