6a88950ca53eaa5a72baf638d53e0ae2570350cd
eyessha <abab484@aucklanduni.ac.nz>
2024-12-25 17:38:51 +1300

    blog data page

---
+++ src/app/blog/blogData.ts
@@ -0,0 +1,19 @@
+const blogData = [
+  {
+    id: 1,
+    title: "Blog Title",
+    date: "10/12/2024",
+    author: "Ayesha Babar",
+    summary: "This is a short summary of the blog post.",
+  },
+  {
+    id: 2,
+    title: "Blog Title",
+    date: "10/12/2024",
+    author: "Ayesha Babar",
+    summary: "This is a short summary of the blog post.",
+  },
+  // Add more blog data as needed
+];
+
+export default blogData;
